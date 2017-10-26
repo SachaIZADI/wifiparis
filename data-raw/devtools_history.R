cef1b32cea5c3c2b716a44d2218427cd96b4b76b
Sacha Izadi <sacha.izadi@polytechnique.edu>
2017-10-26 11:42:45 +0200

    2nd commit

---
+++ data-raw/devtools_history.R
@@ -0,0 +1,11 @@
+devtools::use_data_raw()
+devtools::use_data(wifi_connexion_data)
+devtools::use_data(mapping_site_gps_catsite)
+devtools::use_data(mapping_langues)
+devtools::use_data(mapping_devices)
+
+devtools::use_package("sp")
+devtools::use_data(arrondissements_geojson)
+
+
+# Il faut écrire un .R pour les database
