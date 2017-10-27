df1d883108a5fe0b4fb298d4aba0002f160f3bea
Alex Momeni <alexmomeni@Alexs-MacBook-Pro.local>
2017-10-27 20:48:58 +0200

    Adding packages to devtools_history

--- data-raw/devtools_history.R
+++ data-raw/devtools_history.R
@@ -3,9 +3,16 @@ devtools::use_data(wifi_connexion_data)
 devtools::use_data(mapping_site_gps_catsite)
 devtools::use_data(mapping_langues)
 devtools::use_data(mapping_devices)
-
-devtools::use_package("sp")
 devtools::use_data(arrondissements_geojson)
 
+devtools:: use_package("dplyr")
+devtools:: use_package("tidyr")
+
+devtools:: use_package("leaflet")
+devtools::use_package("sp")
+devtools::use_package("sf")
+devtools:: use_package("geojson")
 
 # Il faut écrire un .R pour les database
+
+
