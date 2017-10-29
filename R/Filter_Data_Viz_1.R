252b795585a3d3a08f4d7dac298b45ff363ffeca
Sacha Izadi <sacha.izadi@polytechnique.edu>
2017-10-29 23:00:02 +0100

    Little correction to take into account the duration filter

--- R/Filter_Data_Viz_1.R
+++ R/Filter_Data_Viz_1.R
@@ -1,14 +1,15 @@
 #' Filter the wifi_connexion_data DB joined with the mapping DB, according to the filters parametrized in the Shiny App
 #'
-#' @param start = a POSIXct, end = a POSIXct, duration_max = an int (default is 7200), districts, = a vector (default is c("All")) , cat_sites = idem, sites = idem, countries = idem, devices = idem
+#' @param start = a POSIXct, end = a POSIXct, duration_min = an int (default is 0), duration_max = an int (default is 7200), districts, = a vector (default is c("All")) , cat_sites = idem, sites = idem, countries = idem, devices = idem
 #' @import tidyr dplyr lubridate
 #' @return a dataframe
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' start<-ymd("2016-09-10")
-#' end<-ymd("2016-09-12")
+#' start<-lubridate::ymd("2016-09-10")
+#' end<-lubridate::ymd("2016-09-12")
+#' duration_min<- 10
 #' duration_max<- 5000
 #' districts<-c(1,2,5,8,"All")
 #' cat_sites<-c("Mairie","Bibliothèque")
@@ -19,13 +20,13 @@
 #' }
 #'
 
-Viz1_Filter <-function(start, end, duration_max=7200, districts=c("All"), cat_sites=c("All"), sites=c("All"), countries=c("All"), devices=c("All")){
+Viz1_Filter <-function(start, end, duration_min=0, duration_max=7200, districts=c("All"), cat_sites=c("All"), sites=c("All"), countries=c("All"), devices=c("All")){
 
   ## data("Data_Viz_1")
   ######## Si jamais il y a un pb, décommenter la ligne du haut. A-t-on vraiment besoin de loader la database ici à chaque fois, car c'est super long si on doit la loader ...
 
   Data_Viz_1_Filter<-Data_Viz_1 %>% filter(date(start_time)>=start & date(start_time)<=end)
-  Data_Viz_1_Filter <- Data_Viz_1_Filter %>% filter(duration<duration_max)
+  Data_Viz_1_Filter <- Data_Viz_1_Filter %>% filter(duration<duration_max & duration_min< duration)
   if (!"All" %in% districts){
     Data_Viz_1_Filter <- Data_Viz_1_Filter %>% filter(Ardt %in% districts)
   }
