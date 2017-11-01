7c4e037a19f0085dc3f6d41bff88402dde8e7eaa
Alex Momeni <alexmomeni@Alexs-MBP.home>
2017-11-01 01:49:39 +0100

    dd

--- R/Map_plot.R
+++ R/Map_plot.R
@@ -16,40 +16,29 @@ Data_Map_plot <- Data_Map_Filter(start, end, duration_min, duration_max, distric
 Data_Map_gps_catsite <- mapping_site_gps_catsite %>%  filter (Site %in% unique(Data_Map_plot$site))
 
 map <- leaflet(arrondissements_geojson) %>%
-
   addProviderTiles(providers$OpenStreetMap.BlackAndWhite) %>%
-
   setView(lng =2.345228, lat = 48.862246, zoom = 12) %>%
-
   addPolygons(color = pal_Ardt, stroke = FALSE,
               smoothFactor = 0, fillOpacity = 0.3) %>%
-
   addPolylines () %>%
-
   addCircleMarkers(stroke = FALSE,
 
                    lng=as.numeric(Data_Map_gps_catsite$y),
                    lat=as.numeric(Data_Map_gps_catsite$x),
                    radius = 5,
 
-                   color = pal_Site(Data_Map_gps_catsite$category_site),
-                   fillColor = pal_Site(Data_Map_gps_catsite$category_site),
+                   color = pal_Site(mapping_site_gps_catsite$category_site),
+                   fillColor = pal_Site(mapping_site_gps_catsite$category_site),
                    fillOpacity = 5,
                    clusterOptions = markerClusterOptions(
                      showCoverageOnHover = FALSE, zoomToBoundsOnClick = TRUE,
                      spiderfyOnMaxZoom = TRUE, removeOutsideVisibleBounds = TRUE,
                      spiderLegPolylineOptions = list(weight = 1.5, color = "#222", opacity =
                                                        0.5), freezeAtZoom = FALSE),
-
-
-                   popup =
-                     paste("Nom:", Data_Map_gps_catsite$Site, "<br>",
+                   popup = paste("Nom:", Data_Map_gps_catsite$Site, "<br>",
                            "Adresse:", Data_Map_gps_catsite$Adresse_Postale, "<br>",
                            "Arrondissement:", Data_Map_gps_catsite$Ardt,"<br>") )
 
 
 return(map)
 }
-
-
-
