479721a3e5baf7b3a2f12619b82005b577b2562f
Alex Momeni <alexmomeni@Alexs-MBP.home>
2017-11-01 01:38:12 +0100

    Fixing bug

--- R/Map_plot.R
+++ R/Map_plot.R
@@ -6,48 +6,57 @@
 #' @export
 
 
-Map_plot <- function(start, end, duration_min=0, duration_max=7200, districts=c("All"), cat_sites=c("All"), sites=c("All"), countries=c("All"), devices=c("All")){
+Map_plot <- function(start, end, duration_min=0, duration_max=7200, districts=c("All"), cat_sites=c("All"), sites=c("All"), countries=c("All"), devices=c("All")) {
 
 pal_Ardt <- colorNumeric("Blues", NULL)
 pal_Site <- colorFactor(c("navy", "red","purple","yellow","white","green"),
                           mapping_site_gps_catsite$category_site)
 
 Data_Map_plot <- Data_Map_Filter(start, end, duration_min, duration_max, districts, cat_sites, sites, countries, devices)
-mapping_site_gps_catsite <- mapping_site_gps_catsite %>%  filter (Site %in% Data_Map_Filter$site)
-
+Data_Map_gps_catsite <- mapping_site_gps_catsite %>%  filter (Site %in% unique(Data_Map_plot$site))
 
 map <- leaflet(arrondissements_geojson) %>%
-
+  
   addProviderTiles(providers$OpenStreetMap.BlackAndWhite) %>%
-
+  
   setView(lng =2.345228, lat = 48.862246, zoom = 12) %>%
-
+  
   addPolygons(color = pal_Ardt, stroke = FALSE,
-              smoothFactor = 0, fillOpacity = 0.3, group="Arrondissement") %>%
-
+              smoothFactor = 0, fillOpacity = 0.3) %>%
+  
   addPolylines () %>%
+  
   addCircleMarkers(stroke = FALSE,
 
-                   lng=as.numeric(mapping_site_gps_catsite$y),
-                   lat=as.numeric(mapping_site_gps_catsite$x),
+                   lng=as.numeric(Data_Map_gps_catsite$y),
+                   lat=as.numeric(Data_Map_gps_catsite$x),
                    radius = 5,
 
-                   color = pal_Site(mapping_site_gps_catsite$category_site),
-                   fillColor = pal_Site(mapping_site_gps_catsite$category_site),
+                   color = pal_Site(Data_Map_gps_catsite$category_site),
+                   fillColor = pal_Site(Data_Map_gps_catsite$category_site),
                    fillOpacity = 5,
-                   clusterOptions = markerClusterOptions(
-                     showCoverageOnHover = FALSE, zoomToBoundsOnClick = TRUE,
-                     spiderfyOnMaxZoom = TRUE, removeOutsideVisibleBounds = TRUE,
-                     spiderLegPolylineOptions = list(weight = 1.5, color = "#222", opacity =
-                                                       0.5), freezeAtZoom = FALSE),
 
                    popup =
-                     paste("Nom:", mapping_site_gps_catsite$Site, "<br>",
-                           "Adresse:", mapping_site_gps_catsite$Adresse_Postale, "<br>",
-                           "Arrondissement:", mapping_site_gps_catsite$Ardt,"<br>") )
+                     paste("Nom:", Data_Map_gps_catsite$Site, "<br>",
+                           "Adresse:", Data_Map_gps_catsite$Adresse_Postale, "<br>",
+                           "Arrondissement:", Data_Map_gps_catsite$Ardt,"<br>") )
 
 
 return(map)
 
 }
+
+start<-lubridate::ymd("2016-09-10")
+end<-lubridate::ymd("2016-09-12")
+duration_min<- 10
+duration_max<- 5000
+districts<-c(1,2,5,8,"All")
+cat_sites<-c("Mairie","Bibliothèque")
+sites<-c("Discothèque des Halles","Bibliothèque Port Royal","Bibliothèque Mouffetard")
+countries<-c("SPAIN","FRANCE")
+devices<-c("smartphone","tablet")
+
+wifiparis::Data_Map_Filter(start, end, duration_min, duration_max, districts, cat_sites, sites, countries, devices) %>% View()
+
+wifiparis:: Map_plot(start, end, duration_min, duration_max, districts, cat_sites, sites, countries, devices)
 
