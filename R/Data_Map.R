e4d348c9a2a412270cbb4a133d9a6a6c3fbbccf9
Sacha Izadi <sacha.izadi@polytechnique.edu>
2017-11-06 23:55:26 +0100

    Last cleaning

--- R/Data_Map.R
+++
@@ -1,8 +0,0 @@
-#' Data_Map : duplicate
-#'
-#' @name Data_Map
-#' @docType data
-#' @author Alex \email{alexandre.momeni@@polytechnique.edu}
-#' @references \url{https://opendata.paris.fr/explore/dataset/utilisations_mensuelles_des_hotspots_paris_wi-fi/?q=&sort=stop_time}{The original link}
-#' @keywords data
-NULL
---
+++ data-raw/Examples_to_test_the_package.R
@@ -0,0 +1,52 @@
+library(dplyr)
+
+start<-lubridate::ymd("2016-09-27")
+end<-lubridate::ymd("2016-09-30")
+duration_min<- 10
+duration_max<- 5000
+districts<-c(1,2,5,8,"All")
+cat_sites<-c("Mairie","Bibliothèque")
+sites<-c("All")
+countries<-c("All")
+devices<-c("smartphone","tablet")
+analysis_axis<-"category_device"
+a_set <- Viz1_Filter(start, end, duration_min, duration_max, districts, cat_sites, sites, countries, devices) %>% number_connexions(start,end,analysis_axis)
+
+Viz1_plot_V2(a_set, start, end, analysis_axis, FALSE)
+
+
+
+start<-lubridate::ymd("2016-09-27")
+end<-lubridate::ymd("2016-09-30")
+duration_min<- 10
+duration_max<- 5000
+districts<-c(1,2,5,8,"All")
+cat_sites<-c("Mairie","Bibliothèque")
+sites<-c("All")
+ countries<-c("All")
+ devices<-c("smartphone","tablet")
+ analysis_axis<-"category_device"
+ hierarchy_view<-FALSE
+ a_set <- Viz1_Filter(start, end, duration_min, duration_max, districts, cat_sites, sites, countries, devices) %>%
+ number_connexions(start,end,analysis_axis)
+
+ Viz3_plot(a_set, analysis_axis, hierarchy_view)
+
+
+
+
+ start<-lubridate::ymd("2016-09-27")
+ end<-lubridate::ymd("2016-09-30")
+ duration_min<- 0
+ duration_max<- 7200
+ districts<-c("20","11","18")
+ cat_sites<-c("Bibliothèque")
+ sites<-c("All")
+ countries<-c("All")
+ devices<-c("smartphone","table")
+ a_set<-Viz1_Filter(start, end, duration_min, duration_max, districts, cat_sites, sites, countries, devices)
+
+ Map_plot(a_set)
+
+
+
