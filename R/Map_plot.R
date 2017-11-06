34358f7b015e7ffe12d99a5a9e459d6998039dfc
Alex Momeni <alexmomeni@Alexs-MacBook-Pro.local>
2017-11-06 23:32:25 +0100

    Updated

--- R/Map_plot.R
+++ R/Map_plot.R
@@ -90,15 +90,3 @@ map <- leaflet(arrondissements_geojson, options = leafletOptions(minZoom = 11, m
   return(map)
 }
 
-start<-lubridate::ymd("2016-09-27")
-end<-lubridate::ymd("2016-09-30")
-duration_min<- 0
-duration_max<- 7200
-districts<-c("20","11","18")
-cat_sites<-c("Bibliothèque")
-sites<-c("All")
-countries<-c("All")
-devices<-c("smartphone","table")
-
-Map_plot(start, end, duration_min, duration_max, districts, cat_sites, sites, countries, devices)
-
