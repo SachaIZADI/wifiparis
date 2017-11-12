a02e2bc5c9718bf9f4920deb36efe8b98ef2fd85
AMIR BENMAHJOUB <amirb@MBP-de-AMIR.eleves.polytechnique.fr polytechnique.fr>
2017-11-12 21:56:35 +0100

    updating cleaning steps

--- data-raw/Data_Cleaning_Steps.R
+++ data-raw/Data_Cleaning_Steps.R
@@ -47,9 +47,144 @@ arrondissements_geojson %>% View()
 #################################################################################
 
 
-#cf. Amir's code.
+######## Creating a database for mapping the sites :
+
+site_data <- mapping_site_gps %>% mutate(category_site=NA)
+site_data$category_site<-NA
+
+######## Creating a word cloud to understand the data :
+
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+# Charger les données comme un corpus
+docs <- Corpus(VectorSource(site_data$Site))
+inspect(docs)
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"))
+
+##Completing the category ## grepl(search, in)
+
+#if site contains Square/Parc/jardin/promenade/Berge --> Espaces verts : Square,Parc,Jardin,Promenade,Berge
+
+site_data$category_site[grepl("Square", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("SQUARE", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("square", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("parc", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("Parc", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("jardin", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("Jardin", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("Promenade", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("Berges de Seine", site_data$Site)] <- "Espace ouvert/vert"
+site_data$category_site[grepl("Berge de Seine", site_data$Site)] <- "Espace ouvert/vert"
+## Special cases  :
+site_data$category_site[site_data$Site== "Allee Vivaldi"] <- "Espace ouvert/vert"
+site_data$category_site[site_data$Site== "TOUR ST JACQUES"] <- "Espace ouvert/vert"
+site_data$category_site[site_data$Site== "Ecole du Breuil"] <- "Espace ouvert/vert"
+site_data$category_site[site_data$Site== "Place de la république coté Ouest"] <- "Espace ouvert/vert"
+site_data$category_site[site_data$Site== "Place de la république coté Est"] <- "Espace ouvert/vert"
+site_data$category_site[site_data$Site== "Esplanade Des Invalides"] <- "Espace ouvert/vert"
 
 
+
+#if site contains bibliothèqe/médiathèque/ discothèque --> Bibliothèque = biliothèque, médiathèque, discothèque
+
+site_data$category_site[grepl("Bibliothèque", site_data$Site)] <- "Bibliothèque"
+site_data$category_site[grepl("bibliothèque", site_data$Site)] <- "Bibliothèque"
+site_data$category_site[grepl("Bibliotheque", site_data$Site)] <- "Bibliothèque"
+site_data$category_site[grepl("bibliothèque", site_data$Site)] <- "Bibliothèque"
+site_data$category_site[grepl("Médiathèque", site_data$Site)] <- "Bibliothèque"
+site_data$category_site[grepl("MEDIATHEQUE", site_data$Site)] <- "Bibliothèque"
+site_data$category_site[grepl("Mediatheque", site_data$Site)] <- "Bibliothèque"
+
+
+## Special cases  :
+site_data$category_site[site_data$Site== "Archives de Paris"] <- "Bibliothèque"
+site_data$category_site[site_data$Site== "Discothèque des Halles"] <- "Bibliothèque"
+
+
+#if site contains Mairie/ Hotel --> Mairie = Mairie and Hotel de Ville
+
+site_data$category_site[grepl("Mairie", site_data$Site)] <- "Mairie"
+site_data$category_site[grepl("mairie", site_data$Site)] <- "Mairie"
+site_data$category_site[grepl("Hotel", site_data$Site)] <- "Mairie"
+
+
+## Special cases  :
+site_data$category_site[site_data$Site== "Maison Communale du 15 ̬me"] <- "Mairie"
+site_data$category_site[site_data$Site== "Maison Communale du 15"] <- "Mairie"
+site_data$category_site[site_data$Site== "DSTI"] <- "Mairie"
+
+
+#if site contains Maison des Associations/Entreprises/Etudiantes/Emploi --> Maison d'Accompagnement
+
+site_data$category_site[grepl("Maison des Associations", site_data$Site)] <- "Maison d'accompagnement"
+## Special cases  :
+site_data$category_site[site_data$Site== "Maison Du Developpement Economique Du 20eme"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "MAISON DES INITIATIVES"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Maison de L'Emploi Du 18eme"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Maison des Initiatives Etudiantes du 06eme"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "MAISON DES ENTREPRISES ET DE L'EMPLOI DU 10eme"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Maison de L'Emploi Du 13eme"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Maison Du Developpement Economique Du 19eme"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Espace Economie Emploi Du 14eme"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "MAISON DES ENTREPRISES ET DE L'EMPLOI DU 10em"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Bourse Du Travail"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Antenne Jeune Orillon"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Maison des Acteurs du Paris durable"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Maison Initiatives Etudiantes"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Antenne Jeune Davout"] <- "Maison d'acompagnement"
+site_data$category_site[site_data$Site== "Conseil de la Jeunesse"] <- "Maison d'acompagnement"
+
+
+
+
+#if site contains Centre/musée --> Centres d'animation et culturel
+
+site_data$category_site[grepl("Centre", site_data$Site)] <- "Centre d'animation et culturel"
+site_data$category_site[grepl("Musee", site_data$Site)] <- "Centre d'animation et culturel"
+site_data$category_site[site_data$Site== "Maison de Victor Hugo"] <- "Centre d'animation et culturel"
+site_data$category_site[site_data$Site== "Carre Baudoin"] <- "Centre d'animation et culturel"
+site_data$category_site[site_data$Site== "Espace des Blancs Manteaux"] <- "Centre d'animation et culturel"
+site_data$category_site[site_data$Site== "Maison de Balzac"] <- "Centre d'animation et culturel"
+site_data$category_site[site_data$Site== "Espace Reuilly"] <- "Centre d'animation et culturel"
+site_data$category_site[site_data$Site== "Atelier de Restauration Photographique"] <- "Centre d'animation et culturel"
+site_data$category_site[site_data$Site== "E.C.A Rive Gauche (Espace commerce Culturel)"] <- "Centre d'animation et culturel"
+site_data$category_site[site_data$Site== "Activites artisanales et creation"] <- "Centre d'animation et culturel"
+
+
+#if site contains sportifs/
+
+site_data$category_site[grepl("Sportif", site_data$Site)] <- "Centre de sport et bien être"
+site_data$category_site[grepl("Stade", site_data$Site)] <- "Centre de sport et bien être"
+site_data$category_site[grepl("Solarium", site_data$Site)] <- "Centre de sport et bien être"
+site_data$category_site[site_data$Site== "HALLE GEORGES CARPENTIER TENNIS DE TABLE"] <- "Centre de sport et bien être"
+site_data$category_site[site_data$Site== "PISCINE GEORGES VALLEREY"] <- "Centre de sport et bien être"
+site_data$category_site[site_data$Site== "HALLE GEORGES CARPENTIER ANNEXE HALL"] <- "Centre de sport et bien être"
+site_data$category_site[site_data$Site== "HALLE GEORGES CARPENTIER"] <- "Centre de sport et bien être"
+site_data$category_site[site_data$Site== "Institut du Judo"] <- "Centre de sport et bien être"
+
+#ENNNNNNNND !!!
+
+# cleaner une ligne de NA et renommer la database :
+
+mapping_site_gps_catsite <- site_data  %>% filter(!is.na(category_site))
+
+## cleaner les categories with factors
+
+mapping_site_gps_catsite$category_site<-as.factor(mapping_site_gps_catsite$category_site)
+save(mapping_site_gps_catsite, file = "mapping_site_gps_catsite",compress = TRUE)
+load("mapping_site_gps_catsite")
+View(mapping_site_gps_catsite)
 
 
 
