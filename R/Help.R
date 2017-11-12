09dbdef319e55b157433afebd9f0ac80d06bdf13
Alex Momeni <alexmomeni@Alexs-MacBook-Pro.local>
2017-11-12 20:00:03 +0100

    Vignettes

--- R/Help.R
+++ R/Help.R
@@ -1,10 +1,13 @@
-#' Package
+#' Wifi Package
 #'
-#' @description This package has the functions and datasets underlying the Shiny App developped for the wifiparis project
+#' The package has the functions and datasets underlying the Shiny App developped for the wifiparis project
 #'
 #' @name wifiparis
 #' @docType package
 #'
 #' @author Amir Benmahjoub <amir.benmahjoub@polytechnique.edu>, Alexandre Momeni <alexandre.momeni@polytechnique.edu>, Sacha Izadi <sacha.izadi@polytechnique.edu>
+
 NULL
+
+
 
