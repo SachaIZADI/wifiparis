e4cd28c169440574f067d4634fcb39a9f318d5dd
Alex Momeni <alexmomeni@Alexs-MacBook-Pro.local>
2017-11-12 18:32:19 +0100

    Help for Package

---
+++ R/Help.R
@@ -0,0 +1,8 @@
+#' Training on R package
+#'
+#' functions : Map_plot: Plots a Leaflet Map of Paris with selected filters
+#'
+#' @name wifiparis
+#' @docType package
+#' @author Amir Benmahjoub <amir.benmahjoub@polytechnique.edu>, Alexandre Momeni <alexandre.momeni@polytechnique.edu>, Sacha Izadi <sacha.izadi@polytechnique.edu>
+NULL
